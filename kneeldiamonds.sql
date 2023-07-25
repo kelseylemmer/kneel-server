fe24bfee6f07a3431bd8718c38441daf725d939f
Kelsey Lemmer <kelseylemmer@gmail.com>
2023-07-25 00:00:59 -0400

    inserted metal, size, style, order info to tables

--- kneeldiamonds.sql
+++ kneeldiamonds.sql
@@ -9,14 +9,14 @@ CREATE TABLE `Sizes`
 (
     `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     `carets` NUMERIC(2,1) NOT NULL,
-    `price` NUMERIC(5,2) NOT NULL
+    `price` NUMERIC(6,2) NOT NULL
 );
 
 CREATE TABLE `Metals`
 (
     `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     `metal` VARCHAR(160) NOT NULL,
-    `price` NUMERIC(5,2) NOT NULL
+    `price` NUMERIC(6,2) NOT NULL
 );
 
 CREATE TABLE `Orders`
@@ -25,12 +25,35 @@ CREATE TABLE `Orders`
     `metal_id` INTEGER NOT NULL,
     `style_id` INTEGER NOT NULL,
     `size_id` INTEGER NOT NULL,
-    `time_stamp` NUMERIC NOT NULL,
-    FOREIGN KEY(`metal_id`) REFERENCES `Metals`(`id`) 
-    FOREIGN KEY(`style_id`) REFERENCES `Styles`(`id`)
-    FOREIGN KEY(`size_id`) REFERENCES `Sizes`(`id`)
+    FOREIGN KEY(`metal_id`) REFERENCES `Metals`(`id`),
+    FOREIGN KEY(`style_id`) REFERENCES `Styles`(`id`),
+    FOREIGN KEY(`size_id`) REFERENCES `Sizes`(`id`),
 );
 
+INSERT INTO `Styles` VALUES (null, 'Classic', 50000);
+INSERT INTO `Styles` VALUES (null, 'Modern', 71000);
+INSERT INTO `Styles` VALUES (null, 'Vintage', 95000);
+
+INSERT INTO `Metals` VALUES (null, 'Sterling Silver', 50000);
+INSERT INTO `Metals` VALUES (null, '14K Gold', 12420);
+INSERT INTO `Metals` VALUES (null, '24K Gold', 73640);
+INSERT INTO `Metals` VALUES (null, 'Platinum', 12589);
+INSERT INTO `Metals` VALUES (null, 'Palladium', 12410);
+
+INSERT INTO `Sizes` VALUES (null, 0.5, 40500);
+INSERT INTO `Sizes` VALUES (null, 0.75, 78200);
+INSERT INTO `Sizes` VALUES (null, 1, 147000);
+INSERT INTO `Sizes` VALUES (null, 1.5, 199700);
+INSERT INTO `Sizes` VALUES (null, 2, 363800);
+
+INSERT INTO `Orders` VALUES (null, 5, 1, 2);
+INSERT INTO `Orders` VALUES (null, 1, 2, 3);
+INSERT INTO `Orders` VALUES (null, 1, 1, 1);
+INSERT INTO `Orders` VALUES (null, 4, 3, 2);
+
+DROP TABLE Metals;
+DROP TABLE Styles;
+DROP TABLE Sizes;
 DROP TABLE Orders;
 
 
--- models/orders.py
+++ models/orders.py
@@ -3,9 +3,8 @@ class Order():
     # Class initializer. It has 5 custom parameters, with the
     # special `self` parameter that every method on a class
     # needs as the first parameter.
-    def __init__(self, id, metal_id, style_id, size_id, time_stamp):
+    def __init__(self, id, metal_id, style_id, size_id):
         self.id = id
         self.metal_id = metal_id
         self.style_id = style_id
         self.size_id = size_id
-        self.time_stamp = time_stamp
